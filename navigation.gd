ae1cf4b2045c70d2b9cb22e9cb9edc22cdffa2f7
Christian Voigt <voigt.cmc@gmail.com>
2024-10-12 16:01:40 +0200

    two scenes

--- navigation.gd
+++ navigation.gd
@@ -4,6 +4,9 @@ extends Node2D  # You can also use CharacterBody2D if needed
 # Player movement speed
 var speed = 200
 
+# Reference to the AnimationPlayer
+@onready var anim_player = $AnimationPlayer
+
 # Called every frame
 func _process(delta):
 	# Handle player movement by keyboard and controller
@@ -16,13 +19,20 @@ func handle_movement(delta):
 	# Handle keyboard input (WASD or arrow keys)
 	if Input.is_key_pressed(KEY_W) or Input.is_key_pressed(KEY_UP):
 		input_vector.y -= 1
-	if Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN):
+		anim_player.play("walking_up")
+	elif Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN):
 		input_vector.y += 1
-	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
+		anim_player.play("walking_down")
+	elif Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
 		input_vector.x -= 1
-	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
+		anim_player.play("walking_left")
+	elif Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
 		input_vector.x += 1
-
+		anim_player.play("walking_right")    
+	else:
+		# Stop the animation if no movement
+		anim_player.stop()
+		
 	# Handle controller input (left analog stick)
 	input_vector.x += Input.get_joy_axis(0, JOY_AXIS_LEFT_X)
 	input_vector.y += Input.get_joy_axis(0, JOY_AXIS_LEFT_Y)
